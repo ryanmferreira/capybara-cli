a1a20842cdabc3ba4af6206b2fa80cf9c4205b0c
CapibaraEngineer <rirogames40000@outlook.com>
2026-09-25 11:23:57 -0300

    constexpr refactor

--- capy.cpp
+++ capy.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const array<string,9> capybara = {
+constexpr array<const string_view,9> capybara = {
     "    /\\__/\\_",
     "   /       \\",
     "  /    0    \\----------_",
@@ -22,12 +22,41 @@ constexpr uint8_t capybaraLines = capybara.size();
 constexpr uint8_t maxPerLine = 32;
 constexpr uint8_t textStartLine = 2;
 
-string fileText;
+// Calculate the longest row of the capybara to align the text properly
+constexpr size_t biggestLine = []() -> size_t {
+    size_t bigLine = 0;
+    for(const auto& line : capybara) {
+        bigLine = line.size();
+    }
+    return bigLine;
+}();
 
-int processParameters(char *argv[]);
+int processParameters(char *argv[])
+{
+    string param = argv[1];
+
+    if (param == "--help" || param == "-h")
+    {
+        cout << "Usage: capybara [text or file]" << endl;
+        cout << "Options:" << endl;
+        cout << " --help    Shows this help menu" << endl;
+        cout << " --version Shows the program version" << endl;
+
+        return 1;
+    }
+
+    if (param == "--version" || param == "-v")
+    {
+        cout << "Capybara CLI v1.0.0" << endl;
+        return 1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
+    string fileText;
     string input;
 
     if (argc > 1)
@@ -59,21 +88,11 @@ int main(int argc, char *argv[])
         input = "Capybara Supremacy!";
     }
 
-    // Calculate the longest row of the capybara to align the text properly
-    size_t biggestLine = 0;
-    for (const auto line : capybara)
-    {
-        if (line.length() > biggestLine)
-        {
-            biggestLine = line.length();
-        }
-    }
-
     // ! We need to refactor this code snippet to break down entire words instead of everything
     // Break the line into multiple lines if it exceeds the maximum characters per line
-    for (int8_t i = 0; i < capybaraLines; i++)
+    for (int8_t i = 0; i < capybaraLines; ++i)
     {
-        cout << capybara[i];
+        cout << capybara.at(i);
 
         int32_t textIndex = i - textStartLine;
         int32_t startChar = textIndex * maxPerLine;
@@ -86,29 +105,6 @@ int main(int argc, char *argv[])
         }
 
         cout << endl;
-    }
-
-    return 0;
-}
-
-int processParameters(char *argv[])
-{
-    string param = argv[1];
-
-    if (param == "--help" || param == "-h")
-    {
-        cout << "Usage: capybara [text or file]" << endl;
-        cout << "Options:" << endl;
-        cout << " --help    Shows this help menu" << endl;
-        cout << " --version Shows the program version" << endl;
-
-        return 1;
-    }
-
-    if (param == "--version" || param == "-v")
-    {
-        cout << "Capybara CLI v1.0.0" << endl;
-        return 1;
     }
 
     return 0;
